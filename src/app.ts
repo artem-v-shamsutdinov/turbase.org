c7b0cea2d10a29d3af61ea2a2a6b5bf98cf6d1ff
russoturisto <russoturisto@gmail.com>
2021-09-07 08:59:37 -0600

    Started reworking for koa.js

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,73 @@
-import fastify from "fastify";
-import router from "./router";
+import Koa from 'koa'
+import Router from 'koa-router'
+import { createWriteStream } from "fs";
 
-const server = fastify({
-  // Logger only for production
-  logger: !!(process.env.NODE_ENV !== "development"),
+const serve = require('koa-static')
+
+const app = new Koa()
+const router = new Router()
+
+router.post('/blog', (_ctx, _next) => {
+
+})
+
+router.post('/code', (_ctx, _next) => {
+
+})
+
+router.post('/contact', (_ctx, _next) => {
+
+})
+
+router.post('/learnRevenue', (_ctx, _next) => {
+
+})
+
+router.post('/learnSecurity', (_ctx, _next) => {
+
+})
+
+router.post('/load', (_ctx, _next) => {
+
+})
+
+router.post('/subscribe', (_ctx, _next) => {
+
+})
+
+router.get('/test', (_ctx, _next) => {
+
+})
+
+app.use(async ctx => {
+  if(ctx.is("blog")) {
+    
+  } else if(ctx.is("code")) {
+    
+  } else if(ctx.is("contact")) {
+    
+  } else if(ctx.is("learnRevenue")) {
+
+  } else if(ctx.is("learnSecurity")) {
+
+  } else if(ctx.is("load")) {
+
+  } else if(ctx.is("subscribe")) {
+    
+  } else if(ctx.is("test")) {
+    
+  } else if(ctx.is("")) {
+    
+  } else if(ctx.is("/")) {
+    
+  } else if(ctx.is("index.html")) {
+    
+  }
+  ctx.body = 'Hello World';
 });
 
-// Middleware: Router
-server.register(router);
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
-export default server;
+export default app;
